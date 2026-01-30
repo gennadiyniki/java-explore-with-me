b8ce2b5b1ca49b3665ac140e5ef23fd9242c0c20
gennadiyniki <nikitkogs@gmail.com>
2026-01-30 12:16:22 +0300

    "Оставил без text block"

--- stats/stats-server/src/main/java/ru/practicum/ewm/server/repository/StatsRepository.java
+++ stats/stats-server/src/main/java/ru/practicum/ewm/server/repository/StatsRepository.java
@@ -11,33 +11,29 @@ import java.util.List;
 public interface StatsRepository extends JpaRepository<EndpointHit, Long> {
 
     // Все посещения
-    @Query("""
-        SELECT new ru.practicum.ewm.stats.dto.ViewStatsDto(
-            h.app,
-            h.uri,
-            COUNT(h.id))
-        FROM EndpointHit h
-        WHERE h.timestamp BETWEEN :start AND :end
-        AND (:uris IS NULL OR h.uri IN :uris)
-        GROUP BY h.app, h.uri
-        ORDER BY COUNT(h.id) DESC
-        """)
+    @Query("SELECT new ru.practicum.ewm.stats.dto.ViewStatsDto(" +
+            "h.app, " +
+            "h.uri, " +
+            "COUNT(h.id)) " +
+            "FROM EndpointHit h " +
+            "WHERE h.timestamp BETWEEN :start AND :end " +
+            "AND (:uris IS NULL OR h.uri IN :uris) " +
+            "GROUP BY h.app, h.uri " +
+            "ORDER BY COUNT(h.id) DESC")
     List<ViewStatsDto> findStats(LocalDateTime start,
                                  LocalDateTime end,
                                  List<String> uris);
 
     // Уникальные посещения
-    @Query("""
-        SELECT new ru.practicum.ewm.stats.dto.ViewStatsDto(
-            h.app,
-            h.uri,
-            COUNT(DISTINCT h.ip))
-        FROM EndpointHit h
-        WHERE h.timestamp BETWEEN :start AND :end
-        AND (:uris IS NULL OR h.uri IN :uris)
-        GROUP BY h.app, h.uri
-        ORDER BY COUNT(DISTINCT h.ip) DESC
-        """)
+    @Query("SELECT new ru.practicum.ewm.stats.dto.ViewStatsDto(" +
+            "h.app, " +
+            "h.uri, " +
+            "COUNT(DISTINCT h.ip)) " +
+            "FROM EndpointHit h " +
+            "WHERE h.timestamp BETWEEN :start AND :end " +
+            "AND (:uris IS NULL OR h.uri IN :uris) " +
+            "GROUP BY h.app, h.uri " +
+            "ORDER BY COUNT(DISTINCT h.ip) DESC")
     List<ViewStatsDto> findStatsUnique(LocalDateTime start,
                                        LocalDateTime end,
                                        List<String> uris);
