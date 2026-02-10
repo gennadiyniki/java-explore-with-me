cd14ac20e2debaae5517ded3d64966b9a3b1564a
gennadiyniki <nikitkogs@gmail.com>
2026-02-10 16:40:47 +0300

    fix comment

--- ewm-stats/server/src/main/java/ru/practicum/explorewithme/stats/server/repository/HitRepository.java
+++ ewm-stats/server/src/main/java/ru/practicum/explorewithme/stats/server/repository/HitRepository.java
@@ -30,13 +30,49 @@ public interface HitRepository extends JpaRepository<Hit, Long> {
                                     @Param("end") LocalDateTime end,
                                     @Param("uris") List<String> uris);
 
-    // Найти по URI в период времени
+    @Query("SELECT new ru.practicum.explorewithme.stats.dto.ViewStats(h.app, h.uri, COUNT(h)) " +
+            "FROM Hit h " +
+            "WHERE h.timestamp BETWEEN :start AND :end " +
+            "AND (:uris IS NULL OR h.uri IN :uris) " +
+            "GROUP BY h.app, h.uri " +
+            "ORDER BY COUNT(h) DESC")
+    List<ViewStats> findStats(@Param("start") LocalDateTime start,
+                              @Param("end") LocalDateTime end,
+                              @Param("uris") List<String> uris);
+
+    @Query("SELECT new ru.practicum.explorewithme.stats.dto.ViewStats(h.app, h.uri, COUNT(DISTINCT h.ip)) " +
+            "FROM Hit h " +
+            "WHERE h.timestamp BETWEEN :start AND :end " +
+            "AND (:uris IS NULL OR h.uri IN :uris) " +
+            "GROUP BY h.app, h.uri " +
+            "ORDER BY COUNT(DISTINCT h.ip) DESC")
+    List<ViewStats> findUniqueStats(@Param("start") LocalDateTime start,
+                                    @Param("end") LocalDateTime end,
+                                    @Param("uris") List<String> uris);
+
+    @Query("SELECT new ru.practicum.explorewithme.stats.dto.ViewStats(h.app, h.uri, COUNT(DISTINCT h.ip)) " +
+            "FROM Hit h " +
+            "WHERE h.timestamp BETWEEN :start AND :end " +
+            "GROUP BY h.app, h.uri " +
+            "ORDER BY COUNT(DISTINCT h.ip) DESC")
+    List<ViewStats> findAllUniqueStats(@Param("start") LocalDateTime start,
+                                       @Param("end") LocalDateTime end);
+
+    @Query("SELECT new ru.practicum.explorewithme.stats.dto.ViewStats(h.app, h.uri, COUNT(DISTINCT h.ip)) " +
+            "FROM Hit h " +
+            "WHERE h.timestamp BETWEEN :start AND :end " +
+            "AND h.uri IN :uris " +
+            "GROUP BY h.app, h.uri " +
+            "ORDER BY COUNT(DISTINCT h.ip) DESC")
+    List<ViewStats> findUniqueStatsByUris(@Param("start") LocalDateTime start,
+                                          @Param("end") LocalDateTime end,
+                                          @Param("uris") List<String> uris);
+
     List<Hit> findAllByUriAndTimestampBetween(String uri, LocalDateTime start, LocalDateTime end);
 
-    // Найти все в период времени
     List<Hit> findAllByTimestampBetween(LocalDateTime start, LocalDateTime end);
 
-    // Найти уникальные по URI, APP и IP в период времени
     @Query("SELECT h FROM Hit h WHERE h.timestamp BETWEEN :start AND :end GROUP BY h.uri, h.app, h.ip")
-    List<Hit> findDistinctByTimestampBetween(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
+    List<Hit> findDistinctByTimestampBetween(@Param("start") LocalDateTime start,
+                                             @Param("end") LocalDateTime end);
 }
