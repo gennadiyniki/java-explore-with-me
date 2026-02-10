afa6aea599049353a3c4b7fe9b09cbe80246fb27
gennadiyniki <nikitkogs@gmail.com>
2026-02-10 14:46:53 +0300

    fix comment

--- ewm-stats/client/src/main/java/ru/practicum/explorewithme/stats/client/RestTemplateConfig.java
+++ ewm-stats/client/src/main/java/ru/practicum/explorewithme/stats/client/RestTemplateConfig.java
@@ -4,17 +4,12 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.DefaultUriBuilderFactory;
 
 @Configuration
 public class RestTemplateConfig {
 
-    private static final String SERVER_URL = "http://localhost:9090";
-
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder
-                .uriTemplateHandler(new DefaultUriBuilderFactory(SERVER_URL))
-                .build();
+        return builder.build();
     }
 }
