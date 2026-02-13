0ca1e7f8fc51269a6325dcf97f7ecbf15bf76943
gennadiyniki <nikitkogs@gmail.com>
2026-02-13 10:00:27 +0300

    Изменил @Data на @Sette и @Getter

--- ewm-main/server/src/main/java/ru/practicum/explorewithme/server/entity/Comment.java
+++ ewm-main/server/src/main/java/ru/practicum/explorewithme/server/entity/Comment.java
@@ -1,16 +1,14 @@
 package ru.practicum.explorewithme.server.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "comments")
-@Data
+@Setter
+@Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
